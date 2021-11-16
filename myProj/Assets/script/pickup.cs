b6db5c8d117338aab9452e507748dd6498ef8e89
Tomas685 <sim94287@gmail.com>
2021-11-16 17:40:59 +0200

    0.01

---
+++ myProj/Assets/script/pickup.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine. UI;
+
+public class pickup : MonoBehaviour
+{
+    public bool isGem;
+    private bool isCollected;
+    public bool isHealthPack;
+    public GameObject pickupEffect;
+    public int coin;
+    public GameObject cointext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin = 0;
+        cointext.GetComponent<Text>().text = "my coinT" + coin; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cointext.GetComponent<Text>().text = "my coinT" + coin;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            if (isGem)
+            {
+                Debug.Log("working");
+                //levelManager.instance.gemsCollected++;
+
+                coin +=5;
+                
+                isCollected = true;
+                Destroy(gameObject);
+                Instantiate(pickupEffect, transform.position, transform.rotation);
+                //UIController.instance.gemUpdateCount();
+            }
+
+            if (isHealthPack)
+            {
+                if (PlayerhealthController.instance.currentHealth != PlayerhealthController.instance.maxHealth)
+                {
+                    PlayerhealthController.instance.HealPlayer();
+                    isCollected = true;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
+
